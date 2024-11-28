6f620596728678dfe8b8db3963d73615f6db9d3e
Thanh Tùng <tungphanminhthanh@gmail.com>
2024-11-28 13:26:10 +0700

    Create Project.cpp

---
+++ Project.cpp
@@ -0,0 +1,253 @@
+#include <bits/stdc++.h>
+#include <fstream>
+#include <string>
+#include <conio.h>
+
+#define KEY_RIGHT 77
+#define KEY_LEFT 75
+#define CLEAR_SCREEN() system("cls")
+
+using namespace std;
+
+struct Student {
+    int stt;
+    string hoTen;
+    float toan, ly, hoa;
+    float diemTB;
+};
+
+void inputStudentData(Student& s, int stt) {
+    s.stt = stt;
+    cout << "+ Ho va Ten: ";
+    getline(cin, s.hoTen);
+    cin.ignore();
+
+    cout << "+ Diem Toan: ";
+    cin >> s.toan;
+    
+    cout << "+ Diem Ly: ";
+    cin >> s.ly;
+    
+    cout << "+ Diem Hoa: ";
+    cin >> s.hoa;
+    s.diemTB = round(((s.toan + s.ly + s.hoa) / 3.0) * 100.0) / 100.0;
+}
+
+bool readStudentData(ifstream& fi, Student& s, int stt) {
+    if (!getline(fi, s.hoTen) || !(fi >> s.toan >> s.ly >> s.hoa)) {
+        return false;
+    }
+    fi.ignore();
+    s.stt = stt;
+    s.diemTB = round(((s.toan + s.ly + s.hoa) / 3.0) * 100.0) / 100.0;
+    return true;
+}
+
+void writeStudentData(ofstream& fo, const Student& s) {
+    fo << s.hoTen << endl
+       << s.toan << endl
+       << s.ly << endl
+       << s.hoa << endl;
+}
+
+void displayStudent(const Student& s) {
+    cout << "|" << setw(4) << s.stt << " | " << setw(20) << right << s.hoTen
+         << " |" << setw(5) << s.toan
+         << " |" << setw(3) << s.ly
+         << " |" << setw(4) << s.hoa
+         << " |" << setw(8) << s.diemTB << " |" << endl;
+}
+
+void displayHeader() {
+    cout << "----------------------------------------------------------" << endl;
+    cout << "| STT |       Ho Ten        | Toan | Ly  | Hoa | Diem TB |" << endl;
+    cout << "----------------------------------------------------------" << endl;
+}
+
+bool compareStudents(const Student& a, const Student& b, int sortChoice) {
+    switch (sortChoice) {
+        case 1: return a.hoTen < b.hoTen;
+        case 2: return a.hoTen > b.hoTen;
+        case 3: return a.diemTB < b.diemTB;
+        case 4: return a.diemTB > b.diemTB;
+        default: return false;
+    }
+}
+
+void readFileData(const string& filename, vector<Student>& students) {
+    ifstream fi(filename);
+    if (!fi.is_open()) {
+        cerr << "Khong the mo file: " << filename << endl;
+        return;
+    }
+    int n;
+    fi >> n;
+    fi.ignore();
+    students.clear();
+    for (int i = 0; i < n; ++i) {
+        Student s;
+        if (readStudentData(fi, s, i + 1))
+            students.push_back(s);
+        else {
+            cerr << "Loi doc du lieu tu file tai dong thu " << i + 1 << endl;
+            break;
+        }
+    }
+    fi.close();
+}
+
+void writeFileData(const string& filename, const vector<Student>& students) {
+    ofstream fo(filename);
+    if (!fo.is_open()) {
+        cerr << "Khong the mo file de ghi: " << filename << endl;
+        return;
+    }
+    fo << students.size() << endl;
+    for (const auto& student : students) {
+        writeStudentData(fo, student);
+    }
+    fo.close();
+}
+
+void displayPaginated(const vector<Student>& students, int entriesPerPage) {
+    int currentPage = 0;
+    int totalPages = (students.size() + entriesPerPage - 1) / entriesPerPage;
+
+    do {
+        CLEAR_SCREEN();
+        displayHeader();
+        for (int i = currentPage * entriesPerPage;
+             i < min((currentPage + 1) * entriesPerPage, (int)students.size()); ++i) {
+            displayStudent(students[i]);
+        }
+        cout << "----------------------------------------------------------" << endl;
+        cout << "Tuy chon:" << endl;
+        cout << "- An nut <- va -> de chuyen trang" << endl;
+        cout << "- An so 0 de quay lai" << endl;
+        int key = getch();
+        if (key == 0 || key == 224) {
+            key = getch();
+            if (key == KEY_LEFT && currentPage > 0) {
+                currentPage--;
+            } else if (key == KEY_RIGHT && currentPage < totalPages - 1) {
+                currentPage++;
+            }
+        } else if (key == '0') {
+            break;
+        }
+    } while (true);
+}
+
+int main() {
+    vector<Student> students;
+    const string filename = "C:\\Users\\Kitsume\\Documents\\Data.txt";
+    int choice;
+    CLEAR_SCREEN();
+    do {
+        cout << "Menu: " << endl;
+        cout << "1. Hien thi danh sach" << endl;
+        cout << "2. Them moi" << endl;
+        cout << "3. Sap xep" << endl;
+        cout << "4. Tim kiem" << endl;
+        cout << "5. Chinh sua" << endl;
+        cout << "6. Xoa" << endl;
+        cout << "0. Thoat" << endl;
+        cin >> choice;
+        CLEAR_SCREEN();
+        switch (choice) {
+            case 1:
+                CLEAR_SCREEN();
+                readFileData(filename, students);
+                if (students.empty()) {
+                    cout << "Danh sach sinh vien trong!" << endl;
+                } else {
+                    displayPaginated(students, 5);
+                }
+                break;
+            case 2:
+                CLEAR_SCREEN();
+                int n;
+                cout << "Nhap so luong hoc sinh can them: ";
+                cin >> n;
+                cin.ignore();
+
+                readFileData(filename, students);
+
+                for (int i = 0; i < n; i++) {
+                    Student newStudent;
+                    cout << "Nhap thong tin sinh vien thu " << students.size() + 1 << ":" << endl;
+                    inputStudentData(newStudent, students.size() + 1);
+                    students.push_back(newStudent);
+                }
+                writeFileData(filename, students);
+                break;
+
+            case 3:
+                CLEAR_SCREEN();
+                int sortChoice;
+                cout << "Sap xep theo:" << endl;
+                cout << "1. Ho Ten tang dan" << endl;
+                cout << "2. Ho Ten giam dan" << endl;
+                cout << "3. Diem TB tang dan" << endl;
+                cout << "4. Diem TB giam dan" << endl;
+                cout << "0. Thoat" << endl;
+                cin >> sortChoice;
+                if (sortChoice != 0) {
+                    sort(students.begin(), students.end(), [&](const Student& a, const Student& b) {
+                        return compareStudents(a, b, sortChoice);
+                    });
+                    displayHeader();
+                    for (const auto& student : students) {
+                        displayStudent(student);
+                    }
+                    writeFileData(filename, students);
+                }
+                break;
+            case 4: {
+                CLEAR_SCREEN();
+                cin.ignore();
+                string name;
+                cout << "Nhap ten can tim: ";
+                getline(cin, name);
+                for (const auto& s : students) {
+                    if (s.hoTen == name) {
+                        displayStudent(s);
+                    }
+                }
+                break;
+            }
+            case 5:
+                CLEAR_SCREEN();
+                int stt;
+                cout << "Nhap STT sinh vien can chinh sua: ";
+                cin >> stt;
+                for (auto& s : students) {
+                    if (s.stt == stt) {
+                        inputStudentData(s, stt);
+                        break;
+                    }
+                }
+                writeFileData(filename, students);
+                break;
+            case 6:
+                CLEAR_SCREEN();
+                cout << "Nhap STT sinh vien can xoa: ";
+                cin >> stt;
+                students.erase(students.begin() + (stt - 1));
+                for (size_t i = 0; i < students.size(); ++i) {
+                    students[i].stt = i + 1;
+                }
+                cout << "Xoa thanh cong!" << endl;
+                writeFileData(filename, students);
+                break;
+            case 0:
+                break;
+            default:
+                cout << "Lua chon khong hop le!" << endl;
+        }
+        CLEAR_SCREEN();
+
+    } while (choice != 0);
+
+    return 0;
+}
